f6d1931f9b326583dd0544603ce62f3c7e2e289b
pen9rum <116377197+pen9rum@users.noreply.github.com>
2023-12-18 00:40:12 +0800

    edited url problems with ccWang

--- GoogleQuery.java
+++ GoogleQuery.java
@@ -2,22 +2,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URISyntaxException;
+
 import java.net.URL;
 import java.net.URLConnection;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -26,146 +19,148 @@ import org.jsoup.select.Elements;
 
 public class GoogleQuery 
 {
-	public String searchKeyword;
-	public String url;
-	public String content;
-	
-	
-	
-	
+ public String searchKeyword;
+ public String url;
+ public String content;
+ 
+ 
+ 
+ 
 
-	
-	public GoogleQuery(String searchKeyword)
-	{
-		this.searchKeyword = searchKeyword;
-		
-		    try {
-		        // 使用 URLEncoder.encode 處理中文關鍵字
-		        String encodeKeyword = java.net.URLEncoder.encode(searchKeyword, "utf-8");
-		        this.url = "https://www.google.com/search?q=" + encodeKeyword + "&oe=utf8&num=20";
-		    } catch (UnsupportedEncodingException e) {
-		        System.out.println(e.getMessage());
-		    }
-		/*try 
-		{
-			// This part has been specially handled for Chinese keyword processing. 
-			// You can comment out the following two lines 
-			// and use the line of code in the lower section. 
-			// Also, consider why the results might be incorrect 
-			// when entering Chinese keywords.
-			String encodeKeyword=java.net.URLEncoder.encode(searchKeyword,"utf-8");
-			this.url = "https://www.google.com/search?q="+encodeKeyword+"&oe=utf8&num=20";
-			
-			// this.url = "https://www.google.com/search?q="+searchKeyword+"&oe=utf8&num=20";
-		}
-		catch (Exception e)
-		{
-			System.out.println(e.getMessage());
-		}*/
-	}
-	
-	private String fetchContent() throws IOException
-	{
-		
-		String retVal = "";
+ 
+ public GoogleQuery(String searchKeyword)
+ {
+  this.searchKeyword = searchKeyword;
+  
+     
+  try 
+  {
+   // This part has been specially handled for Chinese keyword processing. 
+   // You can comment out the following two lines 
+   // and use the line of code in the lower section. 
+   // Also, consider why the results might be incorrect 
+   // when entering Chinese keywords.
+   String encodeKeyword=java.net.URLEncoder.encode(searchKeyword,"utf-8");
+   this.url = "https://www.google.com/search?q="+encodeKeyword+"&oe=utf8&num=20";
+   
+   // this.url = "https://www.google.com/search?q="+searchKeyword+"&oe=utf8&num=20";
+  }
+  catch (Exception e)
+  {
+   System.out.println(e.getMessage());
+  }
+ }
+ 
+ private String fetchContent() throws IOException
+ {
+  
+  String retVal = "";
 
-		URL u = new URL(url);
-		URLConnection conn = u.openConnection();
-		
-		    
-		//set HTTP header
-		conn.setRequestProperty("User-agent", "Chrome/107.0.5304.107");
-		InputStream in = conn.getInputStream();
+  URL u = new URL(url);
+  URLConnection conn = u.openConnection();
+  
+      
+  //set HTTP header
+  conn.setRequestProperty("User-agent", "Chrome/107.0.5304.107");
+  InputStream in = conn.getInputStream();
 
-		InputStreamReader inReader = new InputStreamReader(in, "utf-8");
-		BufferedReader bufReader = new BufferedReader(inReader);
-		String line = null;
+  InputStreamReader inReader = new InputStreamReader(in, "utf-8");
+  BufferedReader bufReader = new BufferedReader(inReader);
+  String line = null;
 
-		while((line = bufReader.readLine()) != null)
-		{
-			retVal += line;
-		}
-		return retVal;
-	}
-	
-	 public ArrayList<ResultItem> query() throws IOException {
-	        if (content == null) {
-	            content = fetchContent();
-	        }
+  while((line = bufReader.readLine()) != null)
+  {
+   retVal += line;
+  }
+  return retVal;
+ }
+ 
+  public ArrayList<ResultItem> query() throws IOException {
+         if (content == null) {
+             content = fetchContent();
+         }
 
-	        ArrayList<ResultItem> resultItems = new ArrayList<>();
+         ArrayList<ResultItem> resultItems = new ArrayList<>();
 
-	        Document doc = Jsoup.parse(content);
-	        Elements lis = doc.select("div");
-			lis = lis.select(".kCrYT");
-	       
+         Document doc = Jsoup.parse(content);
+         Elements lis = doc.select("div");
+   lis = lis.select(".kCrYT");
+        
 
-	        for (Element li : lis) {
-	            try {
-	                String citeUrl = li.select("a").get(0).attr("href").replace("/url?q=", "");
-	                String title = li.select("a").get(0).select(".vvjwJb").text();
-	                
-	                if (title.equals("")) {
-	                    continue;
-	                }
+         for (Element li : lis) {
+             try {
+                 String citeUrl = li.select("a").get(0).attr("href").replace("/url?q=", "");
+                 String title = li.select("a").get(0).select(".vvjwJb").text();
+                 
+                 if (title.equals("")) {
+                     continue;
+                 }
+                 String decodedUrl = URLDecoder.decode(citeUrl, StandardCharsets.UTF_8.toString());
+                 if(decodedUrl.contains("&")){
+                  
+                  int position = decodedUrl.indexOf("&");
+                  if (decodedUrl.charAt(position + 1) == 's' && decodedUrl.charAt(position + 2) == 'a') {
+                      decodedUrl = decodedUrl.substring(0, position);
+                  }
+                  
+                 }
+                 System.out.println("Title: " + title + " , url1: " + decodedUrl);
+                
+                 ResultItem resultItem = new ResultItem(title, decodedUrl);
+                 resultItems.add(resultItem);
 
-	                System.out.println("Title: " + title + " , url: " + citeUrl);
+             } catch (IndexOutOfBoundsException e) {
+                 // Handle the exception if needed
+             }
+         }
 
-	                ResultItem resultItem = new ResultItem(title, citeUrl);
-	                resultItems.add(resultItem);
+         return resultItems;
+     }
+ /*public HashMap<String, String> query() throws IOException
+ {
+  if(content == null)
+  {
+   content = fetchContent();
+  }
 
-	            } catch (IndexOutOfBoundsException e) {
-	                // Handle the exception if needed
-	            }
-	        }
+  HashMap<String, String> retVal = new HashMap<String, String>();
+  
+  /* 
+   * some Jsoup source
+   * https://jsoup.org/apidocs/org/jsoup/nodes/package-summary.html
+   * https://www.1ju.org/jsoup/jsoup-quick-start
+    */
+  
+ /* //using Jsoup analyze html string
+  Document doc = Jsoup.parse(content);
+  
+  //select particular element(tag) which you want 
+  Elements lis = doc.select("div");
+  lis = lis.select(".kCrYT");
+  
+  for(Element li : lis)
+  {
+   try 
+   {
+    String citeUrl = li.select("a").get(0).attr("href").replace("/url?q=", "");
+    String title = li.select("a").get(0).select(".vvjwJb").text();
+    
+    if(title.equals("")) 
+    {
+     continue;
+    }
+    
+    System.out.println("Title: " + title  + " , url: " + citeUrl);
+    
+    //put title and pair into HashMap
+    retVal.put(title, citeUrl);
 
-	        return resultItems;
-	    }
-	/*public HashMap<String, String> query() throws IOException
-	{
-		if(content == null)
-		{
-			content = fetchContent();
-		}
-
-		HashMap<String, String> retVal = new HashMap<String, String>();
-		
-		/* 
-		 * some Jsoup source
-		 * https://jsoup.org/apidocs/org/jsoup/nodes/package-summary.html
-		 * https://www.1ju.org/jsoup/jsoup-quick-start
- 		 */
-		
-	/*	//using Jsoup analyze html string
-		Document doc = Jsoup.parse(content);
-		
-		//select particular element(tag) which you want 
-		Elements lis = doc.select("div");
-		lis = lis.select(".kCrYT");
-		
-		for(Element li : lis)
-		{
-			try 
-			{
-				String citeUrl = li.select("a").get(0).attr("href").replace("/url?q=", "");
-				String title = li.select("a").get(0).select(".vvjwJb").text();
-				
-				if(title.equals("")) 
-				{
-					continue;
-				}
-				
-				System.out.println("Title: " + title  + " , url: " + citeUrl);
-				
-				//put title and pair into HashMap
-				retVal.put(title, citeUrl);
-
-			} catch (IndexOutOfBoundsException e) 
-			{
-//				e.printStackTrace();
-			}
-		}
-		
-		return retVal;
-	} */
+   } catch (IndexOutOfBoundsException e) 
+   {
+//    e.printStackTrace();
+   }
+  }
+  
+  return retVal;
+ } */
 }
--- Main.java
+++ Main.java
@@ -5,57 +5,60 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 
 public class Main 
 {
-	public static void main(String[] args) throws KeyManagementException, NoSuchAlgorithmException 
-	{
-		try 
-		{
-			
-			Scanner scn = new Scanner(System.in);
-			String queryInput = scn.next();
-			GoogleQuery googleQuery = new GoogleQuery(queryInput);
-			
-	        ArrayList<ResultItem> results = googleQuery.query();
+ public static void main(String[] args) throws KeyManagementException, NoSuchAlgorithmException 
+ {
+  try 
+  {
+   
+   Scanner scn = new Scanner(System.in);
+   String queryInput = scn.next();
+   GoogleQuery googleQuery = new GoogleQuery(queryInput);
+   
+         ArrayList<ResultItem> results = googleQuery.query();
 
-	        for (ResultItem resultItem : results) {
-	                System.out.println(resultItem);
-	        }
-	        
-			KeywordList kLst;
-			String urlStr;
-			KeywordCounter counter = null;
-			//File file = new File("input.txt");
-			//Scanner fileSC = new Scanner(file);
-			Scanner sc = new Scanner(System.in);
-			kLst = new KeywordList();
-			for(ResultItem url : results) {
-				
-				counter = new KeywordCounter(url.getUrl());
-				System.out.print(url.getUrl());
-				//fileSC.nextLine();
-				sc.nextLine();
-				while (sc.hasNext()) {
-					
-					String keyword = sc.next();
-					System.out.print(keyword);
-					int count = counter.countKeyword(keyword);
-					float weight = sc.nextFloat();
-					Keyword k = new Keyword(keyword, count, weight);
-					kLst.add(k);
-					
-				}
-				kLst.outputScore();
-				//fileSC.close();
-				
-			}
-		} 
-		catch (IOException e) 
-		{
-			e.printStackTrace();
-		}
-	}
+        /* for (ResultItem resultItem : results) {
+                 System.out.println(resultItem);
+         }*/
+         
+   KeywordList kLst;
+   String urlStr;
+   KeywordCounter counter = null;
+   File file = new File("input.txt");
+   Scanner fileSC = new Scanner(file);
+   //Scanner sc = new Scanner(System.in);
+   kLst = new KeywordList();
+   for(ResultItem url : results) {
+    
+    
+            
+    counter = new KeywordCounter(url.getUrl());
+    fileSC.nextLine();
+    //sc.nextLine();
+    while (fileSC.hasNext()) {
+     
+     String keyword = fileSC.next();
+     System.out.print(keyword);
+     int count = counter.countKeyword(keyword);
+     float weight = fileSC.nextFloat();
+     Keyword k = new Keyword(keyword, count, weight);
+     kLst.add(k);
+     
+    }
+    kLst.outputScore();
+    //fileSC.close();
+    
+   }
+  } 
+  catch (IOException e) 
+  {
+   e.printStackTrace();
+  }
+ }
 }
