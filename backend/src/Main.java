c7a9deb5c47c87283198135873b6e978b59f0679
Chichia-Wang <111306013@g.nccu.edu.tw>
2023-12-30 23:01:12 +0800

    +AroundTheNBA

--- backend/src/Main.java
+++ backend/src/Main.java
@@ -1,33 +1,41 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 
-public class Main 
-{
-	public static void main(String[] args) throws KeyManagementException, NoSuchAlgorithmException
-	{			
-		try {
-			AroundTheNBA aroundTheNBA = new AroundTheNBA();
-			aroundTheNBA.printAroundTheNBA();
-            Scanner scn = new Scanner(System.in);
-            String queryInput = scn.next();
-            int searchValue = scn.nextInt(); 
-            GoogleQuery googleQuery = new GoogleQuery(queryInput,searchValue);
+public class Main {
+    public static void main(String[] args) throws KeyManagementException, NoSuchAlgorithmException {
+        try {
+        	AroundTheNBA aroundTheNBA = new AroundTheNBA();
+    		aroundTheNBA.printAroundTheNBA();
+            DatabasePlayerSearch DBsearch = new DatabasePlayerSearch();
+        	Scanner scn = new Scanner(System.in);
+        	System.out.print("Enter the name to search: ");
+        	String inputName = scn.nextLine();
+        	List<String> matchingNames = DBsearch.searchPlayerByName(inputName);
+        	System.out.println("Matching Names:");
+    		for (String fullName : matchingNames) {
+    			System.out.println(fullName);
+    		}
+        	System.out.print("Enter the name among them and press Enter : ");
+            String queryInput = scn.nextLine();
+        	System.out.print("Enter the searchValue: ");
+            int searchValue = scn.nextInt();
+            
+            // Create a GoogleQuery instance with user input
+            GoogleQuery googleQuery = new GoogleQuery(queryInput, searchValue);
 
             ArrayList<ResultItem> results = googleQuery.query();
-            for(ResultItem result : results) {
-            	System.out.println(result);
+            for (ResultItem result : results) {
+                System.out.println(result);
             }
 
-            KeywordList kLst;
+            // Pass the queryInput to KeywordList constructor
+            KeywordList kLst = new KeywordList(queryInput);
+
             String urlStr;
             KeywordCounter counter = null;
             File file = new File("input.txt");
@@ -37,8 +45,6 @@ public class Main
             if (fileSC.hasNextLine()) {
                 fileSC.nextLine();
             }
-
-            kLst = new KeywordList();
 
             for (ResultItem resultItem : results) {
                 counter = new KeywordCounter(resultItem.getUrl());
@@ -50,26 +56,22 @@ public class Main
                     Keyword k = new Keyword(keyword, count, weight);
                     kLst.add(k);
                 }
-                
+
                 float totalScore = kLst.calculateTotalScore();
                 kLst.addResultItemWithScore(resultItem, totalScore);
                 fileSC = new Scanner(file);
                 // Skip the first line
                 if (fileSC.hasNextLine()) {
                     fileSC.nextLine();
-                }         
-            } kLst.sortAndOutput();
+                }
+            }
+            kLst.sortAndOutput();
             PlayerStats stats = new PlayerStats(kLst.getNBAstatUrl());
             stats.getStats();
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (InterruptedException  e1) {
-			 e1.printStackTrace();
-		}
-		
-		
-		
-			
-		
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
     }
 }
