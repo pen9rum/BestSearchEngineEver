dd1baf946ae9eb008ccd11a91ed8978e5de77ccc
Chichia-Wang <111306013@g.nccu.edu.tw>
2024-01-03 23:33:49 +0800

    repair NewsFinder

--- backend/src/NewsFinder.java
+++ backend/src/NewsFinder.java
@@ -47,7 +47,7 @@ public class NewsFinder {
    		String url = null;	   
    		try 
    		{		
-   			String encodeKeyword=java.net.URLEncoder.encode( "Yahoo NBA news"+searchKeyword,"utf-8");
+   			String encodeKeyword=java.net.URLEncoder.encode( "NBA.com"+searchKeyword,"utf-8");
    			
    			url = "https://www.google.com/search?q="+ encodeKeyword +"&lr=lang_en";
    			Document document = Jsoup.connect(url).get();
@@ -56,7 +56,8 @@ public class NewsFinder {
                if (!searchResults.isEmpty()) {
                    Element firstResult = searchResults.first();
                    String resultUrl = firstResult.select("a[href]").attr("href");
-                   playerUrl = resultUrl + "news";
+                   playerUrl = resultUrl;
+                  // System.out.println(playerUrl);
                }
    		}
    		catch (Exception e)
@@ -66,12 +67,28 @@ public class NewsFinder {
    	}	
    
     public void findNewsWithKeyword() throws IOException, InterruptedException {
-        Document doc = Jsoup.connect(playerUrl).get();
-        Elements newsElements = doc.select("a[href]");
-        
+    	
+    	
+    
+    	 Document doc = Jsoup.connect(playerUrl).get();
+
+    	 for (Element aElement : doc.select("div.PlayerNews_item__10b5O")) {
+             String title = aElement.select("p.PlayerNews_headline__w4cFW").text();
+             String content = aElement.select("p.PlayerNews_update__ntYMq").text();
+             String date = aElement.select("p.PlayerNews_date___Te0H").text();
+             //String absHrefValue = aElement.attr("href");
+             System.out.println("Title: " + title);
+             System.out.println("Content: " + content);
+             System.out.println("Date: " + date);
+             System.out.println("-----------------------------");
+             }
+    	 
+             
+              
         ExecutorService executor = Executors.newFixedThreadPool(10); // 使用固定大小的線程池
 
-        for (Element newsElement : newsElements) {
+      
+		/*for (Element newsElement : newsElements) {
             executor.submit(() -> {
                 String url = newsElement.absUrl("href");
                 try {
@@ -98,7 +115,8 @@ public class NewsFinder {
             NewsItem item = newsWithKeyword.get(i);
             System.out.println("Title: " + item.getTitle() + " | URL: " + item.getUrl() + " | Count: " + item.getKeywordCount());
         }
-        System.out.println("---------------------------------------------------------------------");
+        System.out.println("---------------------------------------------------------------------");*/
+         
     }
 
     private int countOccurrences(String text, String keyword) {
